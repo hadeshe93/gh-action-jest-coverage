55543517b64a1bc783170eb830a5d6e41f58fc97
hadeshe93 <hadeshe93@gmail.com>
2021-06-21 23:53:33 +0800

    feat: 更新代码

--- src/index.js
+++ src/index.js
@@ -5,25 +5,31 @@ const testCommand = 'npm test --';
 // const testCommand = 'jest';
 
 execa
-  .command(`git diff --name-only remotes/origin/main...${branch} | grep .js$`)
+  .command(`git diff --name-only remotes/origin/main...${branch}`)
   .then(({ stdout, stderr }) => {
     if (stderr) {
+      console.error('发生了错误 1');
       throw new Error(stderr);
     }
     return stdout.replace(/\n/g, ' ');
   })
   .then((changedFiles) =>
     execa.command(`${testCommand} --listTests --findRelatedTests ${changedFiles}`).then(({ stdout, stderr }) => {
+      console.log('changedFiles: ', changedFiles);
+      console.log('stdout: ', stdout);
+      console.log('typeof stdout: ', typeof stdout);
       if (stderr) {
+        console.error('发生了错误 2');
         throw new Error(stderr);
       }
       return {
         changedFiles: changedFiles.split(' '),
-        relatedTests: JSON.parse(stdout),
+        relatedTests: stdout.split('\n') || [],
       };
     }),
   )
   .then((ctx) => {
+    console.log('ctx: ', ctx);
     if (ctx.relatedTests.length < 1) {
       // wow, nothing to test!
       return;
@@ -32,6 +38,7 @@ execa
     const testFiles = ctx.relatedTests.map((testFile) => path.relative(process.cwd(), testFile)).join(' ');
     const coverageCommand = `${testCommand} --coverage ${collectCoverageFrom} ${testFiles}`;
     return execa.command(coverageCommand, { stdio: 'inherit' }).catch(() => {
+      console.error('发生了错误 3');
       process.exitCode = 1;
     });
   });
